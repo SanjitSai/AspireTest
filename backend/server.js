98bd1ebd1c433c4a5114ff1c5128e1cecc5d6be2
sujithsai-kalakonda <108214819+sujithsai-kalakonda@users.noreply.github.com>
2023-07-19 06:20:15 +0530

    Update server.js
    Instead of "app.use", I have replaced the "use" with the respective call names.

--- backend/server.js
+++ backend/server.js
@@ -26,18 +26,18 @@ const app = express();
  */
 app.use(cors());
 app.use(express.json());
-app.use('/register', registerRoute);
-app.use('/login', loginRoute);
-app.use('/verify', verifyRoute);
-app.use('/forgotpassword', forgotPasswordRoute);
-app.use('/verifyForgotPassword', verifyforgotRoute);
-app.use('/resetpassword', resetPasswordRoute);
-app.use('/addwork', workExperience);
-app.use("/updatework", workExperience)
-app.use("/addeducation", education);
-app.use("/updateeducation", education);
-app.use("/addskill", skill);
-app.use("/deleteskill", skill)
+app.post('/register', registerRoute);
+app.post('/login', loginRoute);
+app.post('/verify', verifyRoute);
+app.post('/forgotpassword', forgotPasswordRoute);
+app.post('/verifyForgotPassword', verifyforgotRoute);
+app.put('/resetpassword', resetPasswordRoute);
+app.post('/addwork', workExperience);
+app.put("/updatework", workExperience)
+app.post("/addeducation", education);
+app.put("/updateeducation", education);
+app.put("/addskill", skill);
+app.delete("/deleteskill", skill)
 app.use(routes);
 
 const port = process.env.PORT || 3004;
