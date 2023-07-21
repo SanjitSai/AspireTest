c35fe0a77fb1734914f16c64c6028e0b12e570c5
Sanjit <88655352+SanjitSai@users.noreply.github.com>
2023-07-21 13:21:38 +0530

    Updated userService.js. Added timeouts for token validity

--- backend/userService.js
+++ backend/userService.js
@@ -2,36 +2,37 @@ const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 
-/*** Define the user schema using Mongoose
+/**
+ * Define the user schema using Mongoose
  */
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
-  role: String,
-  // Additional fields specific to regular users
-  collegeName: String,
+  role : String,
+  collegeName: String, // Can be multiple , take the recent one
   firstName: String,
   lastName: String,
   otp: String,
+  otpCreatedAt: Date,
   verified: Boolean,
   isBanned: Boolean,
   isVerified_forgot: Boolean,
-  name: String,  
-  city: String,  
-  country: String,  
+  name: String,
+  city: String,
+  country: String,
   bio: String,
-  jwtToken: String,  
-  education: [  
+  jwtToken: String,
+  education: [
     {
       universityName: String,
       branch: String,
       startDate: String,
-      endDate: String, 
+      endDate: String,
     },
   ],
-  workExperiences: [  
+  workExperiences: [
     {
       id: Number,
       companyName: String,
@@ -43,16 +44,18 @@ const userSchema = new mongoose.Schema({
     },
   ],
   skills: [],
-  predefinedSkills: [String],
+  predefinedSkills : [String],
   skillsAddedbyUser: [String]
 });
 
-/*** Create a User model using the user schema
+/**
+ * Create a User model using the user schema
  */
 
 const User = mongoose.model('User', userSchema);
 
-/*** Create a nodemailer transport for sending emails
+/**
+ * Create a nodemailer transport for sending emails
  */
 
 const transporter = nodemailer.createTransport({
@@ -68,15 +71,18 @@ const transporter = nodemailer.createTransport({
 
 const userService = {
 
-  /*** Register a new user
+  /**
+   * Register a new user
    * @param {Object} userData - User registration data
    * @returns {Promise<User>} - The registered user
    * @throws {Error} - If registration fails
    */
 
   registerUser: async (userData) => {
-    const { username, password, confirmPassword, email, collegeName, firstName, lastName } = userData;
+    const { username, password, confirmPassword, email,  collegeName, firstName, lastName } = userData;
 
+    
+    
     if (password !== confirmPassword) {
       throw new Error('Password and confirm password do not match');
     }
@@ -86,16 +92,21 @@ const userService = {
       throw new Error('Username or email already exists');
     }
 
-    const otp = generateOTP();
+   // const otp = generateOTP();
+
+    const otpData = generateOTP();
+   const { otp, otpCreatedAt} = otpData;
 
     const user = new User({
       username,
       password, // TODO: Implement password hashing for security
       email,
+      role : "user",
       collegeName,
       firstName,
       lastName,
       otp,
+      otpCreatedAt, // Set the timestamp when the OTP was created
       verified: false,
       isBanned: false,
       isVerified_forgot: false,
@@ -123,8 +134,65 @@ const userService = {
     console.log(user);
     return user;
   },
-  
-  /*** Verify a user with the provided OTP
+
+  registerAdministrator: async (userData) => {
+    const { username, password, confirmPassword, email,  firstName, lastName } = userData;
+
+    
+    
+    if (password !== confirmPassword) {
+      throw new Error('Password and confirm password do not match');
+    }
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      throw new Error('Username or email already exists');
+    }
+
+    //const otp = generateOTP();
+
+    const otpData = generateOTP();
+   const { otp, otpCreatedAt } = otpData;
+
+    const user = new User({
+      username,
+      password, // TODO: Implement password hashing for security
+      email,
+      role : "admin",
+      firstName,
+      lastName,
+      otp,
+      otpCreatedAt,
+      verified: false,
+      isBanned: false,
+      isVerified_forgot: false,
+      jwtToken: "",
+    });
+
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Registration OTP',
+      text: `Your OTP for registration is: ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+
+    console.log('User registered successfully:');
+    console.log(user);
+    return user;
+  },
+
+  /**
+   * Verify a user with the provided OTP
    * @param {string} otp - The OTP to verify
    * @returns {Promise<User>} - The verified user
    * @throws {Error} - If verification fails
@@ -137,11 +205,25 @@ const userService = {
       throw new Error('User not found');
     }
 
+    // Check if the OTP exists and is not expired (within 30 seconds)
+    if ( Date.now() - user.otpCreatedAt.getTime() > 70000) {
+      console.log("Invalid or expired OTP.")
+      const newOtp = generateOTP() 
+      const { otp, otpCreatedAt } = newOtp;
+      user.otp = otp;
+      user.otpCreatedAt = otpCreatedAt
+
+      await user.save()
+      console.log("New OTP generated. Check your mail")
+      console.log(user.otp)
+    }
+   
     if (otp !== user.otp) {
       throw new Error('Invalid OTP');
     }
 
     user.verified = true;
+    user.otpCreatedAt = undefined;
     await user.save();
 
     console.log('User verified successfully:');
@@ -149,13 +231,14 @@ const userService = {
     return user;
   },
 
-  /*** Log in a user with the provided username and password
+  /**
+   * Log in a user with the provided username and password
    * @param {string} username - The user's username
    * @param {string} password - The user's password
    * @returns {Promise<string>} - The JWT token for the logged-in user
    * @throws {Error} - If login fails
    */
-  
+
   loginUser: async (username, password) => {
     const user = await User.findOne({ username });
 
@@ -167,7 +250,7 @@ const userService = {
       throw new Error('Invalid password');
     }
 
-    if(user.isBanned){
+    if (user.isBanned) {
       throw new Error('User is banned');
     }
 
@@ -190,12 +273,13 @@ const userService = {
     return token;
   },
 
-  /*** Send a password reset OTP to the user's email
+  /**
+   * Send a password reset OTP to the user's email
    * @param {string} usernameOrEmail - The username or email of the user
    * @returns {Promise<User>} - The user for whom the OTP is sent
    * @throws {Error} - If sending the OTP fails
    */
-  
+
   forgotPassword: async (usernameOrEmail) => {
     const user = await User.findOne({
       $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
@@ -205,9 +289,10 @@ const userService = {
       throw new Error('User not found');
     }
 
-    const otp = generateOTP();
-
+    const newOtp = generateOTP() 
+    const { otp, otpCreatedAt } = newOtp;
     user.otp = otp;
+    user.otpCreatedAt = otpCreatedAt
     await user.save();
 
     const mailOptions = {
@@ -230,14 +315,15 @@ const userService = {
     return user;
   },
 
-  /*** Reset a user's password with the provided OTP and new password
+  /**
+   * Reset a user's password with the provided OTP and new password
    * @param {string} username - The username of the user
    * @param {string} existingPassword - The existing password of the user
    * @param {string} newPassword - The new password to set for the user
    * @returns {Promise<User>} - The user with the updated password
    * @throws {Error} - If resetting the password fails
    */
-  
+
   resetPassword: async (username, existingPassword, newPassword) => {
     const user = await User.findOne({ username });
 
@@ -245,7 +331,7 @@ const userService = {
       throw new Error('User not found');
     }
 
-    if(!user.isVerify_forgot===false){
+    if (!user.isVerify_forgot === false) {
       throw new Error('Invalid or expired OTP');
     }
 
@@ -261,10 +347,10 @@ const userService = {
   },
 
   /*** Get the predefined skills from the user schema.
-  *
-  * @returns {Promise<string[]>} - An array of predefined skills.
-  * @throws {Error} - If an error occurs while fetching the predefined skills.
-  */
+    *
+    * @returns {Promise<string[]>} - An array of predefined skills.
+    * @throws {Error} - If an error occurs while fetching the predefined skills.
+    */
   getPredefinedSkills: async () => {
     try {
       // Access the predefinedSkills array from the user schema
@@ -272,12 +358,15 @@ const userService = {
       
       return predefinedSkills;
     } catch (error) {
-      console.error('Error fetching predefined skills:', error);
-      throw new Error('Error fetching predefined skills.');
-    }
+    console.error('Error fetching predefined skills:', error);
+    throw new Error('Error fetching predefined skills.');
   }
-  
+}
+
+
 };
+
+
 
 /**
  * Generate a random OTP
@@ -290,7 +379,10 @@ function generateOTP() {
   for (let i = 0; i < 25; i++) {
     otp += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  return otp;
+  return {
+    otp,
+    otpCreatedAt: new Date(), // Set the timestamp when the OTP was created
+  };
 }
 
 module.exports = {
