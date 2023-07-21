d291e04f536f994a9bdd22fcb849c874585d9320
sujithsai-kalakonda <108214819+sujithsai-kalakonda@users.noreply.github.com>
2023-07-21 14:03:08 +0530

    Update updatingSkills.js by adding "/removeinvalidSkills" and "/addnewSkills"
    Administrator should look into predefinedskills list and can do following:
    1. Should look into skills, and verify whether the newly added skills by user are valid are not:
         1.1 If valid may be can reward the user by points.
         1.2 If not, remove the skill from the list, as well as from the skill sections of the users who had that skill.
    
    2. Can add new skills to the predefiniedSkill list, according to the current industry changes in tech.

--- backend/routes/admintasks/updatingSkills.js
+++ backend/routes/admintasks/updatingSkills.js
@@ -1,1 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const { User, userService } = require("../../userService");
 
+/*** Get the predefined skills list.
+ * @route GET /skills
+ * @group Skills - Operations related to user skills
+ * @returns {object} 200 - Success response with the predefined skills list
+ * @returns {object} 500 - Error response for server error
+ */
+router.get('/', async (req, res) => {
+  try {
+    const predefinedSkills = await userService.getPredefinedSkills(); // Getting predefinedSkills list from UserSchema using getPredefinedSkills function in userService.js
+    
+    res.json({ predefinedSkills }); // displaying predefined skills 
+  } catch (error) {
+    console.error('Error fetching predefined skills:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.put('/', async (req, res) =>{
+  try{
+    const { invalidskills } = req.body;
+    // Retrieve new added skills fom userSchema
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (user.jwtToken === "") {
+      return res.status(404).json({ error: 'Invalid User' })
+    }
+
+    if(user.role !== 'admin'){
+       return res.status(404).json({ error: 'Only admin can make cahnges'})
+    }
+  
+    const newlyAddedSkillsbyUser = await User.schema.path("skillsAddedbyUser").caster.enumValues;
+    const predefinedSkills = await User.schema.path("predefinedSkills").caster.enumValues;
+
+    predefinedSkills = predefinedSkills.filter((skill) => !invalidSkills.includes(skill)); // deleting invalid skills from predefinedSkills list
+    await User.updateOne({}, { $set: { predefinedSkills } }); // saving the changes in db
+
+    // Now we have to delete these invalid skills from user's skill section if they exist.
+    const usersWithInvalidSkills = await User.find({ skills: { $in: invalidSkills } });
+    
+    usersWithInvalidSkills.forEach(async (user) => {
+      user.skills = user.skills.filter((skill) => !invalidSkills.includes(skill));
+      await user.save();
+    });
+
+    // making skillsAddedbyUser as empty list
+    skillsAddedbyUser = []
+    await User.updateOne({}, { $set: { skillsAddedbyUser } });
+    
+  }catch{
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+route.put('/newskills', async (req, res) => {
+  try{
+    const { newSkills } = req.body;
+    // Retrieve new added skills fom userSchema
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (user.jwtToken === "") {
+      return res.status(404).json({ error: 'Invalid User' })
+    }
+
+    if(user.role !== 'admin'){
+       return res.status(404).json({ error: 'Only admin can make cahnges'})
+    }
+    
+    const predefinedSkills = await User.schema.path("predefinedSkills").caster.enumValues;
+    
+    predefinedSkills = predefinedSkills.push.apply(predefinedSkills, newSkills) // Adding new skills to the predefinedSkills list by admin 
+    await User.updateOne({}, { $set: { predefinedSkills } }); // saving the changes in db
+
+  }catch{
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
